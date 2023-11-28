e2bdc0bfed77278a9931a5b3d2664998b1bd3c46
Abhijit <abhijitshendagescoe@gmail.com>
2023-11-28 20:52:39 +0530

    Fixed failures in sensor test cases
    Removed unused imports

--- src/tests/sensorTests.py
+++ src/tests/sensorTests.py
@@ -1,7 +1,5 @@
 import unittest
-from unittest.mock import patch
 
-from pyrsistent import s
 from sensor import Sensor  # Assuming the code is in a file named sensor.py
 
 class TestSensor(unittest.TestCase):
